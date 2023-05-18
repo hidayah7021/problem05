adde8849f148399abc4d7463c107f1343e21b98e
hidayah7021 <22007021@myrp.edu.sg>
2023-05-18 15:04:33 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22007021/problem05/ModuleDetails.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id22007021.problem05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModuleDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module_details);
+    }
+}
